ac4ee4ecf07d7480ebe000058e60ad1356682d97
BuildTools <unconfigured@null.spigotmc.org>
2021-10-07 19:38:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/parkshare/CurrentPostingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.parkshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentPostingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_postings2);
+    }
+}
---
+++ app/src/main/java/com/example/parkshare/MyPostingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.parkshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPostingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_postings);
+    }
+}
